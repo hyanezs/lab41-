ec762c3402c3b1cb72fa4492d533e67e40711192
EHU <a@a.com>
2022-11-09 16:53:23 +0100

    second lab

--- src/main/java/modelo/bean/LoginBean.java
+++ src/main/java/modelo/bean/LoginBean.java
@@ -1,8 +1,24 @@
 package modelo.bean;
 
+import java.util.Date;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.SelectEvent;
+
 public class LoginBean {
 	private String nombre;
 	private String password;
+	private Date fecha;
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
 
 	public String getNombre() {
 		return nombre;
@@ -21,10 +37,19 @@ public class LoginBean {
 	}
 
 	public String comprobar() {
+		if (nombre.length() != password.length()) {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage("Error: La longitud del nombre y de la contraseña son diferentes."));
+			return null;
+		}
 		if (nombre.equals("pirata")) {
 			return "error";
 		} else {
 			return "ok";
 		}
+	}
+
+	public void onDateSelect(SelectEvent event) {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Fecha escogida: " + event.getObject()));
 	}
 }
